62e4b645930c5a1afa883a3bfc9004a1eeb2e83d
Vince Allen <vince@foldifoldi.com>
2013-10-02 06:55:18 -0400

    Adds Twitter and Wordnik.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var connect = require('connect'),
     io = require('socket.io').listen(1337),
-    Twit = require('twit');
+    Twit = require('twit'),
+    restclient = require('node-restclient'),
+    sys = require("sys");
 
 connect(connect.static(__dirname + '/public')).listen(8000);
+
+// insert your Wordnik API info below
+var getNounsURL = "http://api.wordnik.com/v4/words.json/randomWords?" +
+                  "minCorpusCount=1000&minDictionaryCount=10&" +
+                  "excludePartOfSpeech=proper-noun,proper-noun-plural,proper-noun-posessive,suffix,family-name,idiom,affix&" +
+                  "hasDictionaryDef=true&includePartOfSpeech=noun&limit=2&maxLength=12&" +
+                  "api_key=";
 
 var T = new Twit({
     consumer_key:         '...'
@@ -12,9 +21,19 @@ var T = new Twit({
 })
 
 io.sockets.on('connection', function(socket) {
-  socket.on('setData', function(data) {
+  socket.on('searchTweets', function(data) {
 
+    restclient.get(getNounsURL, function(reply) {
 
-    socket.emit('dataSet', {id: data.id});
+      socket.emit('nounsReceived', reply);
+
+      if (reply.length) {
+        T.get('search/tweets', { q: reply[0].word + ' since:2013-10-01', count: 1 }, function(err, reply) {
+          socket.emit('tweetsSearched', reply);
+        })
+      }
+
+    }, 'json');
+
   });
 });
