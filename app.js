e18351271aff55acacce933aa85645fbaed620dd
Vince Allen <vince@foldifoldi.com>
2013-10-06 18:00:25 -0400

    Reads credentials from config.json

--- app.js
+++ app.js
@@ -2,26 +2,45 @@ var connect = require('connect'),
     io = require('socket.io').listen(1337),
     Twit = require('twit'),
     restclient = require('node-restclient'),
-    sys = require("sys");
+    sys = require("sys"),
+    fs = require("fs");
 
 connect(connect.static(__dirname + '/public')).listen(8000);
 
-// insert your Wordnik API info below
-var getNounsURL = "http://api.wordnik.com/v4/words.json/randomWords?" +
-                  "minCorpusCount=1000&minDictionaryCount=10&" +
-                  "excludePartOfSpeech=proper-noun,proper-noun-plural,proper-noun-posessive,suffix,family-name,idiom,affix&" +
-                  "hasDictionaryDef=true&includePartOfSpeech=noun&limit=2&maxLength=12&" +
-                  "api_key=";
-
-var T = new Twit({
-    consumer_key:         '...'
-  , consumer_secret:      '...'
-  , access_token:         '...'
-  , access_token_secret:  '...'
-})
+var config, getNounsURL, T;
 
 io.sockets.on('connection', function(socket) {
+
+  // store credentials in config.json
+  var file = __dirname + '/config.json';
+
+  fs.readFile(file, 'utf8', function (err, data) {
+    if (err) {
+      console.log('Error: ' + err);
+      return;
+    }
+    config = JSON.parse(data);
+
+    getNounsURL = "http://api.wordnik.com/v4/words.json/randomWords?" +
+        "minCorpusCount=1000&minDictionaryCount=10&" +
+        "excludePartOfSpeech=proper-noun,proper-noun-plural,proper-noun-posessive,suffix,family-name,idiom,affix&" +
+        "hasDictionaryDef=true&includePartOfSpeech=noun&limit=2&maxLength=12&" +
+        "api_key=" + config.wordnik;
+
+    T = new Twit({
+        consumer_key:         config.twitter_consumer_key
+      , consumer_secret:      config.twitter_consumer_secret
+      , access_token:         config.twitter_access_token
+      , access_token_secret:  config.twitter_access_token_secret
+    });
+
+  });
+
   socket.on('searchTweets', function(data) {
+
+    if (!getNounsURL || !T) {
+      return;
+    }
 
     restclient.get(getNounsURL, function(reply) {
 
